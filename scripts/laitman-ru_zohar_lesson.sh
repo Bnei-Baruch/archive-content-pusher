1ce066fba54334e085864421e40bd77a223c5aaf
Edo Shor <edoshor@gmail.com>
2018-11-14 21:03:01 +0200

    Scripts - cron jobs for zohar lessons

---
+++ scripts/laitman-ru_zohar_lesson.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set +e
+set -x
+
+BASE_DIR="/sites/archive-content-pusher"
+TIMESTAMP="$(date '+%Y%m%d%H%M%S')"
+LOG_FILE="$BASE_DIR/logs/laitman-ru/zohar_lesson_$TIMESTAMP.log"
+
+cd ${BASE_DIR} && source bin/activate && ./wp-autopost.py --zohar > ${LOG_FILE} 2>&1
+
+WARNINGS="$(egrep -c "(WARNING|ERROR)" ${LOG_FILE})"
+
+if [ "$WARNINGS" = 0 ];then
+        echo "No warnings"
+        exit 0
+fi
+
+echo "Errors in laitman.ru zohar lesson" | mail -s "ERROR: laitman.ru zohar lesson" -r "mdb@bbdomain.org" -a ${LOG_FILE} edoshor@gmail.com
+
+find "${BASE_DIR}/logs/laitman-ru" -type f -mtime +7 -exec rm -rf {} \;
